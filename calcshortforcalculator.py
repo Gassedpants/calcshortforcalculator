35a9bdd3c3da9dacf81ca93fdb8c292e46913fd3
Muhamad Asyraf bin Muhamad Faizal <2401543J@student.tp.edu.sg>
2026-04-06 06:46:55 +0000

    Added main file (Still has operation errors)

---
+++ calcshortforcalculator.py
@@ -0,0 +1,130 @@
+import tkinter as tk
+
+#Setting button values
+button_values = [
+    ["AC", "+/-", "%", "÷"],
+    ["7", "8", "9", "x"],
+    ["4", "5", "6", "-"],
+    ["1", "2", "3", "+"],
+    ["0", ".", "√", "="]
+    ]
+
+right_symbols = ["÷", "x", "-", "+", "="]
+top_symbols = ["AC", "+/-", "%" ]
+
+row_count =len(button_values)
+column_count =len(button_values[0])
+
+colour1 = '#FEBD6F'
+colour2 = '#712E1B'
+colour3 = '#411F25'
+colour4 = '#D75A24'
+colour5 = '#FF8032'
+
+window = tk.Tk()
+window.title("Calculator")
+window.resizable(False, False)
+
+frame = tk.Frame(window)
+label = tk.Label(frame, text="0", font=("Arial", 45), background=colour1,
+                      foreground=colour3, anchor = "e")
+
+label.grid(row = 0, column= 0, columnspan=column_count, sticky="we")
+
+for row in range(row_count):
+    for column in range(column_count):
+        value = button_values[row][column]
+        button = tk.Button(frame, text=value, font = ("Arial", 30),
+                           width=column_count-1, height =1,
+                           command = lambda value=value: button_clicked(value))
+        
+        if value in top_symbols:
+            button.config(foreground=colour2, background=colour3)
+        elif value in right_symbols:
+            button.config(foreground =colour5, background=colour2)
+        else:
+            button.config(foreground=colour2, background=colour1)
+        button.grid(row=row+1, column=column)
+
+frame.pack()
+
+#A+B A-B A*B A/B
+A = "0"
+operator = None
+B = None
+
+def clear_all():
+    global A, operator, B
+    A = "0"
+    operator = None
+    B = None
+
+def remove_zero_decimal(num):
+    if num % 1 == 0:
+        num = int(num)
+    return str(num)
+
+def button_clicked(value):
+    global top_symbols, right_symbols, A, operator, B, label
+    
+    if value in right_symbols:
+        if value == "=":
+            if A is not None and operator is not None:
+                B = label["text"]
+                numA = float(A)
+                numB = float(B)
+                
+                if operator == "+":
+                    label["text"] = remove_zero_decimal(numA + numB)
+                elif operator == "-":
+                    label["text"] = remove_zero_decimal(numA - numB)
+                elif operator == "x":
+                    label["text"] = remove_zero_decimal(numA * numB)
+                elif operator == "÷":
+                    label["text"] = remove_zero_decimal(numA / numB)
+                    
+                clear_all()
+                
+        elif value== "+-x÷":
+            if operator is None:
+                A = label["text"]
+                label["text"] = "0"
+                B ="0"
+                
+            operator = value 
+            
+            
+    elif value in top_symbols:
+        if value == "AC":
+            clear_all()
+            label["text"] = "0"
+        elif value == "+/-":
+            result = float(label["text"]) * -1
+            label["text"] = remove_zero_decimal(result)
+        elif value == "%":
+            result = float(label["text"]) / 100
+            label["text"] = remove_zero_decimal(result)
+    else: #digits or .
+        if value == ".":
+            if value not in label["text"]:
+                label["text"] += value
+        elif value in "0123456789":
+            if label["text"] == "0":
+                label["text"] = value
+            else:
+                label["text"] += value 
+        
+
+window.update()
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+window_x = int((screen_width/2)-(window_width/2))
+window_y = int((screen_height/2)-(window_height/2))
+
+window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
+
+
+window.mainloop()
